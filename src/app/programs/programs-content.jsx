c9f90c7991677a201e1302b5d3a74f0a262f6e6d
suha jaafar <suhajaafar0@gmail.com>
2025-09-26 09:11:12 +0300

    add style program

--- src/app/programs/programs-content.jsx
+++ src/app/programs/programs-content.jsx
@@ -24,31 +24,36 @@ export default function ProgramsContent() {
 	}, []);
 
 	return (
-		<div className='container'>
+		<div className='container mt-6'>
 			<div className='row'>
 				{programs?.map((data, id) => (
-					<>
-						<div className='col-xl-4 col-lg-6 col-md-6 mb-30' key={id}>
-							<div className='blog__two-item'>
-								<div className='blog__two-item-image'>
-									<img src={data.img} alt='image' />
-									<div className='blog__two-item-image-date'>
-										<h5>{data.date}</h5>
-										<span>{data.month}</span>
-									</div>
+					<div className='col-xl-4 col-lg-6 col-md-6 mb-30' key={id}>
+						<div className='blog__two-item'>
+							<div className='blog__two-item-image'>
+								<div className='initiatives__area-item-image'>
+									<Image
+										src={data.image?.url}
+										alt={data.title}
+										width={400}
+										height={300}
+									/>
 								</div>
-								<div className='blog__two-item-content'>
-									<h6>Post by - {data.postby}</h6>
-									<h4>
-										<a href={`/blog-details/${data.id}`}>{data.title}</a>
-									</h4>
-									<a className='simple-btn' href={`/blog-details/${data.id}`}>
-										<i className='far fa-chevron-right'></i>Read More
-									</a>
+								<div className='blog__two-item-image-date'>
+									<h5>{data.date}</h5>
+									<span>{data.month}</span>
 								</div>
 							</div>
+							<div className='blog__two-item-content'>
+								<h6>Post by - {data.postby}</h6>
+								<h4>
+									<a href={`/blog-details/${data.id}`}>{data.title}</a>
+								</h4>
+								<a className='simple-btn' href={`/blog-details/${data.id}`}>
+									<i className='far fa-chevron-right'></i>Read More
+								</a>
+							</div>
 						</div>
-					</>
+					</div>
 				))}
 			</div>
 		</div>
